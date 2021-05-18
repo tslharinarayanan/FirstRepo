88b8f2bf27961b6bb61fea812cd98a7916b42d76
Hari Narayanan Muthuswamy <harinarayanan.m@thirdware.com>
2021-05-18 15:29:32 +0530

    Final Commit

--- src/com/smaple/pro/NewClas.java
+++ src/com/smaple/pro/NewClas.java
@@ -2,7 +2,7 @@ package com.smaple.pro;
 
 public class NewClas {
 	public static void main(String[] args) {
-		System.out.println("Main Class");
+		System.out.println("");
 		System.out.println("New line");
 	}	
 }
